7b6469072b311fb16e8dff8425c53424f16ab3c4
Terence Wong <tybw.dev@gmail.com>
2015-08-16 08:43:27 +1200

    Some updates

--- simpleaws/src/Webfit/AWS/AppBundle/Controller/DefaultController.php
+++ simpleaws/src/Webfit/AWS/AppBundle/Controller/DefaultController.php
@@ -5,13 +5,61 @@ namespace Webfit\AWS\AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
+
+    protected $aws = array();
+    private   $config = array();
+    protected $container;
+
+
     public function mainAction(Request $request)
     {
-        $this->get('awsService');
-
+        $this->config_aws();
         return $this->render('WebfitAWSAppBundle:Main:index.html.twig', []);
+    }
+
+    public function ec2StatusAction(Request $request, $profile)
+    {
+        $this->config_aws();
+        $result = array();
+
+        if (isset($this->aws[$profile])) {
+            $result = $this->aws[$profile]->ec2InstanceStatus();
+        }
+
+        return new JsonResponse($result);
+    }
+
+    public function ec2Action(Request $request, $profile)
+    {
+        $this->config_aws();
+        $result = array();
+
+        if (isset($this->aws[$profile])) {
+            $result = $this->aws[$profile]->ec2InstanceDetails();
+        }
+        
+        return new JsonResponse($result);
+    }
+
+    private function config_aws()
+    {
+
+        $profiles = $this->container->getParameter('aws_profile');
+        $regions  = $this->container->getParameter('aws_region');
+        $versions = $this->container->getParameter('aws_version');
+
+        for ($i = 0; $i < count($profiles); $i++) {
+            $this->config[$profiles[$i]] = array(
+                'profile' => $profiles[$i],
+                'region'  => $regions[$i],
+                'version' => $versions[$i]
+            );
+            $this->aws[$profiles[$i]] = $this->get('awsService');
+            $this->aws[$profiles[$i]]->setProfile($this->config[$profiles[$i]]);
+        }
     }
 }
--- simpleaws/src/Webfit/AWS/AppBundle/Service/AWSService.php
+++ simpleaws/src/Webfit/AWS/AppBundle/Service/AWSService.php
@@ -10,21 +10,98 @@ use Aws\Rds\RdsClient;
 use Aws\AutoScaling\AutoScalingClient;
 use Aws\ElasticLoadBalancing\ElasticLoadBalancingClient;
 use Aws\CloudFront\CloudFrontClient;
+use Aws\Api\DateTimeResult;
+use Guzzle\Service\Resource;
 
-class AWSService {
+class AWSService
+{
 
-    public function __construct(EntityManager $em, ContainerInterface $container, $config = null)
+    public $aws;
+    public $ec2;
+    public $s3;
+    public $as;
+    public $elb;
+    public $cf;
+    public $rds;
+
+    public function __construct(EntityManager $em, ContainerInterface $container)
     {
         $this->container = $container;
         $this->em = $em;
+    }
 
-        if ($config != null) {
-            return null;
+    public function setProfile($config)
+    {
+
+        $this->aws = new Sdk($config);
+
+        if ($this->aws) {
+            $this->ec2 = $this->aws->createEc2();
+            $this->s3  = $this->aws->createS3();
+            $this->as  = $this->aws->createAutoScaling();
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function ec2InstanceStatus()
+    {
+        $response = $this->ec2->describeInstanceStatus(array(
+            'DryRun' => false
+            /*
+            'InstanceIds' => array()
+            'Filters' => array(
+                array(
+                    'Name' => 'string',
+                    'Values' => array('string', ... ),
+                ),
+            ),
+            'NextToken' => 'string',
+            'MaxResults' => integer,
+            */
+        ));
+
+        $result = array();
+        $groups = $response->get('InstanceStatuses');
+
+        foreach ($groups as $group) {
+            $result[$group['InstanceId']] = $group['InstanceState']['Name'];
         }
 
-        $aws       = new Sdk($config);
-        $ec2       = $aws->createEc2();
-        $s3        = $aws->createS3();
-        $autoscale = $aws->createAutoScaling();
+        return $result;
     }
+
+    public function ec2InstanceDetails()
+    {
+        $response = $this->ec2->describeInstances(array(
+            'DryRun' => false
+        ));
+
+        $result = array();
+        $groups = $response->get('Reservations');
+        foreach ($groups as $group) {
+            $tags = array();
+            foreach ($group['Instances'][0]['Tags'] as $tag) {
+                if (strcmp($tag['Key'], 'aws:autoscaling:groupName') == 0) {
+                    $tags[] = $tag['Value'];
+                }
+            }
+
+            $instanceId = $group['Instances'][0]['InstanceId'];
+            $result[$instanceId] = array(
+                'InstanceId' => $instanceId,
+                'ImageId'    => $group['Instances'][0]['ImageId'],
+                'Status'     => $group['Instances'][0]['State']['Name'],
+                'PubicIp'    => $group['Instances'][0]['PublicIpAddress'],
+                'PrivateIp'  => $group['Instances'][0]['PrivateIpAddress'],
+                'LaunchAt'   => $group['Instances'][0]['LaunchTime']->format('Y-m-d H'),
+                'Tag'        => join(',', $tags)
+            );
+        }
+
+        return $result;
+    }
+
 }
