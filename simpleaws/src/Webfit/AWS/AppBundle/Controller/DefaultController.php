fd1fab77bab95ea359f543e5049953085c25f826
Terence Wong <tybw.dev@gmail.com>
2015-08-18 00:17:09 +1200

    Built 3 APIs

--- simpleaws/src/Webfit/AWS/AppBundle/Controller/DefaultController.php
+++ simpleaws/src/Webfit/AWS/AppBundle/Controller/DefaultController.php
@@ -12,10 +12,11 @@ use Webfit\AWS\AppBundle\Entity\Schedule;
 class DefaultController extends Controller
 {
 
+    protected $container;
     protected $aws = array();
     private   $config = array();
-    protected $container;
-
+    private   $now;
+    private   $em;
 
     public function mainAction(Request $request)
     {
@@ -51,19 +52,137 @@ class DefaultController extends Controller
     {
         $result = array();
 
-        $now = new \DateTime;
+        $this->init();
 
-        $em = $this->getDoctrine()->getEntityManager();
-        $query = $em->createQueryBuilder()
+        $query = $this->em->createQueryBuilder()
             ->select('s')
             ->from('WebfitAWSAppBundle:Schedule', 's')
             ->where('s.scheduleAt >= :currentTime')
-            ->setParameter('currentTime', $now->format("%Y-%m-%d %H:%M:%S"))
+            ->setParameter('currentTime', $this->now->modify('-10 minutes'))
+            ->getQuery();
+
+        $schedules = $query->getResult();
+        foreach ($schedules as $schedule) {
+            $result[] = array(
+                'rowId'      => $schedule->getRowId(),
+                'asGroup'    => $schedule->getAsGroup(),
+                'quantity'   => $schedule->getQuantity(),
+                'createAt'   => $schedule->getCreateAt(),
+                'scheduleAt' => $schedule->getScheduleAt(),
+                'runAt'      => $schedule->getRunAt()
+            );
+        }
+
+        return new JsonResponse($result);
+    }
+
+    /**
+     * No 2 schedules should be less than 5 minutes apart
+     */
+    public function scheduleAtAction(Request $request)
+    {
+        $this->init();
+
+        $asGroup      = $request->get('asGroup');
+        $quantity     = $request->get('quantity');
+        $scheduleDate = $request->get('date');
+        $scheduleTime = ($request->get('time') == null) ? "0:00am" : $request->get('time');
+
+        if (! $this->checkDuplicateSchedule($asGroup, $scheduleDate, $scheduleTime)) {
+
+            $this->saveSchedule($asGroup, $quantity, $scheduleOn);
+
+            $result = array(
+                'returnCode' => 0,
+                'message'    => sprintf('Schedule saved (%s %s %s %s)', $asGroup, $quantity, $scheduleDate, $scheduleTime)
+            );
+        } else {
+            $result = array(
+                'returnCode' => 1,
+                'message'    => 'Requested schedule is too near to existing schedules'
+            );
+        }
+
+        return new JsonResponse($result);
+    }
+
+    public function scheduleNowAction(Request $request)
+    {
+        $this->init();
+
+        $asGroup    = $request->get('asGroup');
+        $quantity   = $request->get('quantity');
+        $scheduleOn = clone $this->now;
+        $scheduleDate = $scheduleOn->format('Y-m-d');
+        $scheduleTime = $scheduleOn->format('g:ia');
+
+        if (! $this->checkDuplicateSchedule($asGroup, $scheduleDate, $scheduleTime)) {
+            $this->saveSchedule($asGroup, $quantity, $scheduleOn);
+            if ($this->updateAutoScalingGroup()) {
+                $result = array(
+                    'returnCode' => 0,
+                    'message'    => 'Desired number of EC2 intances has been set.'
+                );
+            } else {
+                $result = array(
+                    'returnCode' => 2,
+                    'message'    => 'Problem encountered in chaning auto-scaling group.'
+                );
+            }
+        } else {
+            $result = array(
+                'returnCode' => 1,
+                'message'    => 'Requested schedule is too near to existing schedules'
+            );
+        }
+
+        return new JsonResponse($result);
+    }
+
+    private function updateAutoScalingGroup()
+    {
+        return true;
+    }
+
+    private function checkDuplicateSchedule($asGroup, $scheduleDate, $scheduleTime)
+    {
+
+        $scheduleOn = new \DateTime(sprintf("%s %s", $scheduleDate, $scheduleTime));
+
+        $lowerBound = clone $scheduleOn;
+        $upperBound = clone $scheduleOn;
+        $lowerBound->modify('-5 minutes');
+        $upperBound->modify('+5 minutes');
+
+        $query = $this->em->createQueryBuilder()
+            ->select('s')
+            ->from('WebfitAWSAppBundle:Schedule', 's')
+            ->where('s.scheduleAt >= :lowerBound AND s.scheduleAt <= :upperBound AND s.asGroup = :asGroup')
+            ->setParameter('lowerBound', $lowerBound)
+            ->setParameter('upperBound', $upperBound)
+            ->setParameter('asGroup',    $asGroup)
             ->getQuery();
 
         $schedules = $query->getResult();
 
-        return new JsonResponse($result);
+        return (count($schedules) > 0) ? true : false;
+    }
+
+    private function saveSchedule($asGroup, $quantity, $scheduleOn)
+    {
+        $runAt = clone $scheduleOn;
+        $runAt->modify(Schedule::ADVANCED_BY);
+
+        $schedule = new Schedule;
+        $schedule->setRowId(uniqid());
+        $schedule->setAsGroup($asGroup);
+        $schedule->setQuantity($quantity);
+        $schedule->setCreateAt($this->now);
+        $schedule->setScheduleAt($scheduleOn);
+        $schedule->setRunAt($runAt);
+
+        $this->em->persist($schedule);
+        $this->em->flush();
     }
 
     private function config_aws()
@@ -82,5 +201,11 @@ class DefaultController extends Controller
             $this->aws[$profiles[$i]] = $this->get('awsService');
             $this->aws[$profiles[$i]]->setProfile($this->config[$profiles[$i]]);
         }
+    }
+
+    private function init() {
+        $this->now = new \DateTime;
+        $this->em = $this->getDoctrine()->getEntityManager();
+        return true;
     }
 }
--- simpleaws/src/Webfit/AWS/AppBundle/Entity/Schedule.php
+++ simpleaws/src/Webfit/AWS/AppBundle/Entity/Schedule.php
@@ -19,13 +19,24 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
  */
 class Schedule
 {
+    const ADVANCED_BY = '-3 hours';
+
     /**
      * @var integer
      *
-     * @ORM\Column(name="rowid", type="integer")
+     * @ORM\Column(name="rowid", type="string")
      * @ORM\Id
      */
     private $rowid;
+
+    /**
+     * Auto-scaling group
+     *
+     * @var string
+     *
+     * @ORM\Column(name="as_group", type="string", nullable=false)
+     */
+    private $asGroup;
 
     /**
      * Quantity
@@ -61,4 +72,142 @@ class Schedule
      */
     protected $run_at;
 
+
+    /**
+     * Set rowid
+     *
+     * @param integer $rowid
+     * @return Schedule
+     */
+    public function setRowid($rowid)
+    {
+        $this->rowid = $rowid;
+
+        return $this;
+    }
+
+    /**
+     * Get rowid
+     *
+     * @return integer 
+     */
+    public function getRowid()
+    {
+        return $this->rowid;
+    }
+
+    /**
+     * Set asGroup
+     *
+     * @param string $asGroup
+     * @return Schedule
+     */
+    public function setAsGroup($asGroup)
+    {
+        $this->asGroup = $asGroup;
+
+        return $this;
+    }
+
+    /**
+     * Get asGroup
+     *
+     * @return string 
+     */
+    public function getAsGroup()
+    {
+        return $this->asGroup;
+    }
+
+    /**
+     * Set quantity
+     *
+     * @param integer $quantity
+     * @return Schedule
+     */
+    public function setQuantity($quantity)
+    {
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    /**
+     * Get quantity
+     *
+     * @return integer 
+     */
+    public function getQuantity()
+    {
+        return $this->quantity;
+    }
+
+    /**
+     * Set createAt
+     *
+     * @param \DateTime $createAt
+     * @return Schedule
+     */
+    public function setCreateAt($createAt)
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreateAt()
+    {
+        return $this->createAt;
+    }
+
+    /**
+     * Set scheduleAt
+     *
+     * @param \DateTime $scheduleAt
+     * @return Schedule
+     */
+    public function setScheduleAt($scheduleAt)
+    {
+        $this->scheduleAt = $scheduleAt;
+
+        return $this;
+    }
+
+    /**
+     * Get scheduleAt
+     *
+     * @return \DateTime 
+     */
+    public function getScheduleAt()
+    {
+        return $this->scheduleAt;
+    }
+
+    /**
+     * Set run_at
+     *
+     * @param \DateTime $runAt
+     * @return Schedule
+     */
+    public function setRunAt($runAt)
+    {
+        $this->run_at = $runAt;
+
+        return $this;
+    }
+
+    /**
+     * Get run_at
+     *
+     * @return \DateTime 
+     */
+    public function getRunAt()
+    {
+        return $this->run_at;
+    }
 }
